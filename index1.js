cc218f8e4e93f0b7d5a04a629b163a0577aefd99
darecallad <Ashane199080706@hotmail.com.tw>
2021-08-30 16:36:26 -0700

    updated

--- index1.js
+++ index1.js
@@ -3,15 +3,16 @@ const mongoose = require("mongoose");
 const customer1 = require("./router/customer1");
 const app = express();
 
+// connect to Database
+mongoose
+  .connect("mongodb://localhost/api/customer")
+  .then(() => console.log("Connect to Database...."))
+  .catch((ex) => console.log("cannot connect to Database...", ex));
+
 // app use
 app.use(express.json());
-app.use("/api/customer1", customer1);
-// connect to database
-mongoose
-  .connect("mongodb://localhost/cAPI")
-  .then(() => console.log("connect to MongoDB"))
-  .catch((ex) => console.error("cannot connect to MongoDB...", ex));
+app.use("/api/customer", customer1);
 
-//setting port environment
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`listen on ${port}...`));
+//port
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`listening on prot ${port}...`));
--- router/customer1.js
+++ router/customer1.js
@@ -1,57 +1,76 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Joi = require("joi");
-const router = express.router();
+const router = express.Router();
 
-// createSchema
-
+// Create Schema
 const customerSchema = new mongoose.Schema({
-  name: { type: String, required: true, minlength: 2, maxlength: 10 },
-  phone: { type: String, required: true },
-  isGold: boolean,
+  name: { type: String, required: true, minlength: 2 },
+  phone: { type: String, required: true, minlength: 5 },
+  isGold: Boolean,
 });
 
-//create Model
+//model
 const Customer = mongoose.model("Customer", customerSchema);
 
 //validate
 function validateCustomer(customer) {
   const schema = {
-    name: Joi.string().required().min(2).max(10),
-    phone: Joi.string().required(),
+    name: Joi.string().required().min(2),
+    phone: Joi.string().required().min(5),
   };
   return Joi.validate(customer, schema);
 }
 
-//get,post,put,delete
-
-//get
+// get post put delete
+// get
 router.get("/", async (req, res) => {
   const customer = await Customer.find().sort("name");
   res.send(customer);
 });
-
-//get id
+//get:id
 router.get("/:id", async (req, res) => {
-  const customer = await Customer.findById(req.params.id);
-  if (!customer) return res.status(404).send("Cannot find this customer... ");
+  const customer = await Customer.findById(req.param.id);
+  if (!customer) return res.status(404).send("cannot find the customer");
+
   res.send(customer);
 });
-
 //post
 router.post("/", async (req, res) => {
-  //Validate
   const { error } = validateCustomer(req.body);
-  if (error) return res.status(400).rend(error.details[0].message);
+  if (error) return res.status(400).send("ERROR");
 
-  // new customer
-  let customer = Customer({ name: req.body.name }, { phone: req.body.phone });
-  customer = await Customer.save();
-
+  let customer = new Customer(
+    { name: req.body.name },
+    { phone: req.body.phone },
+    { isGold: req.body.isGold }
+  );
+  customer = await customer.save();
   res.send(customer);
 });
 
 //put
-router.put("/:id", (req, res) => {
-  Customer.findByIdAndUpdate(req.body.id);
+router.put("/:id", async (req, res) => {
+  const { error } = validateCustomer(req.body);
+  if (error) return res.status(400).send("ERROR");
+
+  const customer = await Customer.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    { phone: req.body.phone },
+    { isGold: req.body.isGold },
+    { new: true }
+  );
+  if (!customer) return res.status(404).send("Request 404");
+  res.send(customer);
 });
+
+//delete
+router.delete("/:id", async (req, res) => {
+  const customer = await Customer.findByIdAndRemove(req.params.id);
+  if (!customer) return res.status(404).send("Bad Request");
+
+  res.send(customer);
+});
+
+module.exports = router;
